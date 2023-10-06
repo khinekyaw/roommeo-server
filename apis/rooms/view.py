8cbc2f69b31246ec2aa1c305232ef213b982b603
daniel <danielrio126@gmail.com>
2023-10-06 22:29:06 +0630

    add room image upload

--- apis/rooms/view.py
+++ apis/rooms/view.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from apis.permissions import IsHost, ReadOnly
-from rooms.models import Room, Amenity
+from rooms.models import Room, RoomImage
 from .serializers import RoomSerializer, RoomDetailSerializer
 
 
@@ -10,8 +11,19 @@ class RoomViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsHost | ReadOnly]
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    parser_classes = (MultiPartParser, FormParser)
 
     def get_serializer_class(self):
         if self.action == "retrieve":
             return RoomDetailSerializer
         return RoomSerializer
+
+    def perform_create(self, serializer):
+        # Save the room instance
+        room = serializer.save()
+
+        # Handle image uploads
+        images_data = self.request.data.getlist("images")
+
+        for image_data in images_data:
+            RoomImage.objects.create(room=room, image=image_data)
