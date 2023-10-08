30596680d42182a5a1816a757416fba195195c3b
daniel <danielrio126@gmail.com>
2023-10-08 10:54:46 +0630

    update room viewset methods

--- apis/rooms/view.py
+++ apis/rooms/view.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import serializers
 
 from apis.permissions import IsHost, ReadOnly
 from rooms.models import Room, RoomImage
-from .serializers import RoomSerializer, RoomDetailSerializer
+from .serializers import RoomSerializer, RoomDetailSerializer, RoomImageSerializer
 
 
 class RoomViewSet(viewsets.ModelViewSet):
@@ -19,11 +20,25 @@ class RoomViewSet(viewsets.ModelViewSet):
         return RoomSerializer
 
     def perform_create(self, serializer):
-        # Save the room instance
         room = serializer.save()
-
-        # Handle image uploads
         images_data = self.request.data.getlist("images")
 
         for image_data in images_data:
-            RoomImage.objects.create(room=room, image=image_data)
+            image_serializer = RoomImageSerializer(data={"image": image_data})
+            if image_serializer.is_valid():
+                image_serializer.save(room=room)
+            else:
+                raise serializers.ValidationError(image_serializer.errors)
+
+    def perform_update(self, serializer):
+        room = serializer.save()
+        images_data = self.request.data.getlist("images")
+
+        room.images.all().delete()
+
+        for image_data in images_data:
+            image_serializer = RoomImageSerializer(data={"image": image_data})
+            if image_serializer.is_valid():
+                image_serializer.save(room=room)
+            else:
+                raise serializers.ValidationError(image_serializer.errors)
